a3cffe0ef1d960bab4472018bb30d458e4fdcdec
underdogdude <underdogbright@gmail.com>
2021-06-19 16:27:07 +0800

    ADD Header

---
+++ js/script-custom.js
@@ -0,0 +1,48 @@
+
+/* Manage Class Funtions https://www.sitepoint.com/add-remove-css-class-vanilla-js/ */
+function addClass(e,l){var elements=document.querySelectorAll(e);for(var s=0;s<elements.length;s++)elements[s].classList.add(l)}function removeClass(e,l){var elements=document.querySelectorAll(e);for(var s=0;s<elements.length;s++)elements[s].classList.remove(l)}
+
+
+function openFullScreenNav() {
+  document.getElementById("fullScreenNav").style.width = "60%";
+  document.getElementById("sideBgNav").style.width = "110px";
+}
+function closeFullScreenNav() {
+  document.getElementById("fullScreenNav").style.width = "0%";
+  document.getElementById("sideBgNav").style.width = "0%";
+}
+
+/* -------  Mobile Menu:: Dropdown Toggle ------- */
+document.addEventListener(
+    "click",
+    function (event) {
+        if (event.target.matches(".site-toggle")) {
+            if (event.target.classList.contains("active")) {
+                removeClass(".site-toggle, .site-nav-m", "active");
+                removeClass("#masthead", "show-nav");
+            } else {
+                addClass(".site-toggle, .site-nav-m", "active");
+                addClass("#masthead", "show-nav");
+            }
+        }
+        if (event.target.matches("#site-nav-m .nav-item-has-children > svg")) {
+            console.log('click');
+            if (event.target.parentNode.classList.contains("active")) {
+                event.target.parentNode.classList.remove("active");
+            } else {
+                event.target.parentNode.classList.add("active");
+            }
+        }
+    },
+    false
+);
+/* -------  Mobile Menu:: Add Icon Dropdown Toggle ------- */
+document
+    .querySelectorAll("#site-nav-m .nav-item-has-children")
+    .forEach((e) => {
+        e.insertAdjacentHTML(
+        "beforeend",
+        '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-plus" viewBox="0 0 20 20"><path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/></svg>'
+        );
+    }
+);
